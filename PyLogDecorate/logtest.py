946f39a76bdaa92c6e691542b0cf4f3a4106ff31
offlinehacker <jakahudoklin@gmail.com>
2011-09-13 14:30:07 -0700

    Added some more advanced test/example

--- PyLogDecorate/logtest.py
+++ PyLogDecorate/logtest.py
@@ -9,8 +9,11 @@ class test1(object):
             
     @LogCall({"subdecorate": True, "tracename": "tracetest"})
     def test(self):
-        print "znj"
+        pass
     
+    @LogCall({"subdecorate": True, "tracename": "tracetest"})  
+    def test2(self):
+        self.logger.debug("Inside base class!")
 
 @LogClass()
 class test2(test1):
@@ -19,8 +22,20 @@ class test2(test1):
     
     def test(self):
         self.logger.debug("Inside funtion.")
+        self.test2()
+        
+@LogClass()   
+class test3(test2):
+    def __init__(self):
+        pass
+    
+    def test(self):
+        self.logger.debug("Inside funtion2.")
+        self.test2()
     
     
 test()
-b=test2()
+b=test3()
+c=test2()
 b.test()
+c.test()
